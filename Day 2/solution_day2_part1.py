ed7d0b86277a8d57f76253eed0003e7c046c54b3
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2023-04-17 18:02:17 +0200

    Added test and results for day 2 part 1

--- Day 2/solution_day2_part1.py
+++ Day 2/solution_day2_part1.py
@@ -1,24 +1,33 @@
-def intcode_interpreter(memory):
-    memory[1] = 12
-    memory[2] = 2
-    pc = 0
-    while memory[pc] != 99:
-        op = memory[pc]
-        a = memory[memory[pc + 1]]
-        b = memory[memory[pc + 2]]
-        c = memory[pc + 3]
-        if op == 1:
-            memory[c] = a + b
-        elif op == 2:
-            memory[c] = a * b
+def run_program(program):
+    pc = 0  # program counter
+    while True:
+        opcode = program[pc]
+        if opcode == 1:
+            a, b, c = program[pc+1:pc+4]
+            program[c] = program[a] + program[b]
+        elif opcode == 2:
+            a, b, c = program[pc+1:pc+4]
+            program[c] = program[a] * program[b]
+        elif opcode == 99:
+            break  # halt
         else:
-            print("Error: unknown opcode")
-            return
+            raise ValueError(f"Invalid opcode {opcode} at position {pc}")
         pc += 4
-    return memory[0]
+    return program
 
+# Read the input
 with open("input", "r") as f:
-    memory = list(map(int, f.read().strip().split(",")))
+    input_str = f.read().strip()
 
-print(intcode_interpreter(memory))
+# Parse the input string into a list of integers
+program = list(map(int, input_str.split(",")))
 
+# Restore "1202 program alarm" state
+program[1] = 12
+program[2] = 2
+
+# Run the program
+program = run_program(program)
+
+# Print the output
+print(program[0])
--- Day 2/test_day2_part1.py
+++ Day 2/test_day2_part1.py
@@ -1,15 +1,9 @@
 import pytest
 
-from solution_day2_part1 import intcode_interpreter
+from solution_day2_part1 import run_program
 
-def test_intcode_interpreter():
-    assert intcode_interpreter([1, 0, 0, 0, 99]) == 2
-    assert intcode_interpreter([2, 3, 0, 3, 99]) == 6
-    assert intcode_interpreter([2, 4, 4, 5, 99, 0]) == 9801
-    assert intcode_interpreter([1, 1, 1, 4, 99, 5, 6, 0, 99]) == 30
-
-def test_input():
-    with open("input", "r") as f:
-        memory = list(map(int, f.read().strip().split(",")))
-    assert intcode_interpreter(memory) == 3101844
-
+def test_run_program():
+    assert run_program([1,0,0,0,99]) == [2,0,0,0,99]
+    assert run_program([2,3,0,3,99]) == [2,3,0,6,99]
+    assert run_program([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
+    assert run_program([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
